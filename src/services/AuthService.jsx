224d46d0c0d4c42ed3036caebfd2397e18f95001
trilce <mquispe.231ds26@istta.edu.pe>
2025-07-16 00:26:30 -0500

    mis

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
  //Usamos variable de entorno
 const API = axios.create({
- baseURL: import.meta.env.VITE_API_URL,
+baseURL: import.meta.env.VITE_API_URL,
 withCredentials: true,
 });
 
