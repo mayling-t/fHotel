2362ed36fda8764835a1f734f1e7d00051bf6ad5
Flor Marina <fchoqque.231ds11@istta.edu.pe>
2025-07-16 21:03:09 -0500

    nueva actualizaciones

--- src/services/ClienteService.jsx
+++ src/services/ClienteService.jsx
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 //const API_URL = 'http://127.0.0.1:8000';
-
 export const obtenerClientePorUsuario = async (usuarioId) => {
-  const response = await axios.get(`${API_URL}/cliente/usuario/${usuarioId}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_URL}/cliente/usuario/${usuarioId}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      alert("No se encontró cliente vinculado al usuario. Por favor, registra tu perfil de cliente.");
+      // Puedes redirigir o tomar otra acción
+    }
+    throw error;  // vuelve a lanzar para que lo capture el catch del form
+  }
 };
